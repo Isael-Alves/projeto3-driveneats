bf44532ed8d7db6677c4d49c6fd6806222984929
= <isael.alves@tucurui.ufpa.br>
2022-06-11 03:37:35 -0300

    Order completion message OK

--- java/script.js
+++ java/script.js
@@ -1,21 +1,33 @@
+let Food;
+let Drink;
+let Dessert;
+
+let FoodCost;
+let DrinkCost;
+let DessertCost;
+let TotalCost;
+
 function SelectFood(Card) {
     const FoodSelected = document.querySelector(".Food .Selected");
     const Check = Card.querySelector(".Icon-check");
+    Food = Card.querySelector('h4').innerHTML;
 
     if (FoodSelected !== null) {
         FoodSelected.classList.remove("Selected");
         const FoodCheck = FoodSelected.querySelector(".Icon-check");
-        FoodCheck.classList.add("Hidden");  
+        FoodCheck.classList.add("Hidden");
     }
 
     Card.classList.add("Selected");
     Check.classList.remove("Hidden");
     EnableButton();
+    String_for_number(Card);
 }
 
 function SelectDrink(Card) {
     const DrinkSelected = document.querySelector(".Drink .Selected");
     const Check = Card.querySelector(".Icon-check");
+    Drink = Card.querySelector('h4').innerHTML;
 
     if (DrinkSelected !== null) {
         DrinkSelected.classList.remove("Selected");
@@ -26,11 +38,13 @@ function SelectDrink(Card) {
     Card.classList.add("Selected");
     Check.classList.remove("Hidden");
     EnableButton();
+    String_for_number(Card);
 }
 
 function SelectDessert(Card) {
     const DessertSelected = document.querySelector(".Dessert .Selected");
     const Check = Card.querySelector(".Icon-check");
+    Dessert = Card.querySelector('h4').innerHTML;
 
     if (DessertSelected !== null) {
         DessertSelected.classList.remove("Selected");
@@ -41,12 +55,40 @@ function SelectDessert(Card) {
     Card.classList.add("Selected");
     Check.classList.remove("Hidden");
     EnableButton();
+    String_for_number(Card);
 }
 
-function EnableButton(){
-const counter = document.querySelectorAll(".Selected").length
-    if (counter === 3){
+function EnableButton() {
+    const counter = document.querySelectorAll(".Selected").length;
+    if (counter === 3) {
         document.querySelector(".Button").classList.add("Hidden")
         document.querySelector(".Button-confirm-order").classList.remove("Hidden");
     }
+}
+
+function String_for_number(Card) {
+    const Value = Card.querySelector("h5").innerHTML;
+    const NewValue = Value.replace("R$", "").replace(",", ".");
+    const ClassofCard = Card.classList[0];
+
+    if (ClassofCard === "Card-Food") {
+        FoodCost = Number(NewValue) * 100;
+    }
+    if (ClassofCard === "Card-Drink") {
+        DrinkCost = Number(NewValue) * 100;
+    }
+    if (ClassofCard === "Card-Dessert") {
+        DessertCost = Number(NewValue) * 100;
+    }
+}
+
+function CompleteBuy() {
+    TotalCost = ((FoodCost + DrinkCost + DessertCost)/100).toFixed(2);
+    const FinishMsg =
+        `Olá, gostaria de fazer o pedido:\n  
+      - Prato: ${Food}\n
+      - Bebida: ${Drink}\n
+      - Sobremesa: ${Dessert}\n
+        Total: R$ ${TotalCost.replace(".",",")}\n`
+    alert(FinishMsg);
 }
